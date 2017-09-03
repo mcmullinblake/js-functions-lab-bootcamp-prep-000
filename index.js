83189b992f6d4baa75f674efdf7de68f31a03f7c
blake mcmullin <mcmullin.blake@gmail.com>
2017-09-03 21:11:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidayTo(holiday, name) {
-  return `${holiday}, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
